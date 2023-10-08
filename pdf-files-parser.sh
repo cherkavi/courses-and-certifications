bf3195a7bb56ff65bbcbd2a124a299b445b7d8bc
Vitalii Ch <vitalii.cherkashyn@gmail.com>
2023-10-08 22:45:14 +0200

    Update pdf-files-parser.sh

--- pdf-files-parser.sh
+++ pdf-files-parser.sh
@@ -1,11 +1,12 @@
-## parse all image-files and retrieve text of the certificate
 # go to the folder with GDrive files
 destination_file=$HOME/out.txt
 touch $destination_file
 for each_file in `ls .`; do
     link="https://drive.google.com/file/d/${each_file}/view?usp=drive_link" 
-    text_cr=`ocr $each_file | grep -A 2  "successfully completed" | grep -v "successfully completed"`
+    text_cr=`ocr $each_file | grep -A 3  "successfully completed" | grep -v "successfully completed" | grep -v " minutes " | grep -v " hours " | grep -v " days " `
     text=`echo $text_cr | tr "\n" ' '`
+    echo $text_cr
+    echo $text
     echo "[${text}](${link})" >> $destination_file
 done
 cat $destination_file
